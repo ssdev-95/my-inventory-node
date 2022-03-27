fb666f4c476dcebeda65050874e4c139e2f98dfc
Salomao S <souza95salomao@gmail.com>
2022-03-27 12:30:37 -0400

    [UPDATE] Add update product route

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { AddProductController } from "./controllers/AddProductController"
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController"
 import { RetrieveProductController } from "./controllers/RetrieveProductController"
 import { DeleteProductController } from "./controllers/DeleteProductController"
+import { UpdateProductController } from "./controllers/UpdateProductController"
 import { isAuthenticated, isRefreshing } from "./middlewares"
 
 const router = Router()
@@ -29,6 +30,12 @@ router.delete(
 	"/products",
 	isAuthenticated,
 	new DeleteProductController().handle
+)
+
+router.put(
+	"/products",
+	isAuthenticated,
+	new UpdateProductController().handle
 )
 
 router.post(
